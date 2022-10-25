d4e3804d86b7c4dd2ac6df8dc8c1aae14f3115ce
root <root@LAPTOP-TSTO18D1.localdomain>
2022-10-24 21:53:42 -0700

    small fix

--- sketch.js
+++ sketch.js
@@ -1,4 +1,5 @@
-var peer, conn, origPeerId, destPeerId
+var peer, origPeerId, destPeerId
+var outConn, inConn
 var recMouseX = 0, recMouseY = 0
 
 function setup() {
@@ -12,31 +13,38 @@ function setup() {
 
 		destPeerId = prompt("Your peer ID is:\n" + id + "\n\nEnter collaborator's peer ID:")
 
-		conn = peer.connect(destPeerId)
-		conn.on('data', function (data) {
+		console.log("Connection sent")
+		outConn = peer.connect(destPeerId)
+	});
+
+	peer.on('connection', function (dataConnection) {
+		inConn = dataConnection
+		console.log("Connection received")
+
+		inConn.on('data', function (data) {
 			console.log('Data received: ' + data)
 			recMouseX = data.x
 			recMouseY = data.y
 		});
 	});
 
-	peer.on('connection', function (dataConnection) {
-		console.log("Connected")
-	});
-
 }
 
 function draw() {
-	if (conn) {
-		conn.send({ x: mouseX, y: mouseY })
-		console.log(conn.open)
-	}
-
-
 	background(0)
 	noStroke()
 	fill(255)
-
+	
 	ellipse(mouseX, mouseY, 20, 20)
-	ellipse(recMouseX, recMouseY, 20, 20)
+
+	if (outConn) {
+		outConn.send({ x: mouseX, y: mouseY })
+		console.log("outConn: " + outConn.open)
+	}
+
+	if (inConn) {
+		console.log("inConn: " + inConn.open)
+		ellipse(recMouseX, recMouseY, 20, 20)
+	}
+
 }
